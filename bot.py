1a0cf837518d68ba114419cbcf75c3aeba3b63ca
Andrey P <39740137+anpodin@users.noreply.github.com>
2018-07-07 18:25:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,21 @@
 import telebot
 
 bot = telebot.TeleBot("607967748:AAGGCS5t8TK7aKxZUeav2sgwcPTmkpVJDls")
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    if message.text == "Hi":
-        bot.send_message(message.from_user.id, "Hello! I am HabrahabrExampleBot. How can i help you?")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+bot.reply_to(message, "Привет! Я бот Андрея, как твои дела?")
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+
+    if message.text == "Привет":
+        bot.reply_to(message, "Привет! Как твои дела?")
     
-    elif message.text == "How are you?" or message.text == "How are u?":
-        bot.send_message(message.from_user.id, "I'm fine, thanks. And you?")
+    elif message.text == "Как дела?" or message.text == "How are u?":
+        bot.reply_to(message, "Отлично!")
     
     else:
-        bot.send_message(message.from_user.id, "Sorry, i dont understand you.")
+        bot.reply_to(message, "Извини, я очень глуп, ничего не понимаю, кроме привет и как дела...")
 
 bot.polling(none_stop=True, interval=0)
