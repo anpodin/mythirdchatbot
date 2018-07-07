9c992677da9f217b9fc93fe74bd06049c090780a
Andrey P <39740137+anpodin@users.noreply.github.com>
2018-07-07 14:09:56 +0300

    Create init.py

---
+++ init.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import telebot
+
+bot = telebot.TeleBot("607967748:AAGGCS5t8TK7aKxZUeav2sgwcPTmkpVJDls")
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет! Я третий бот Андрея. Спроси мена как дела.")
+    
+    elif message.text == "Как дела?" or message.text == "How are u?":
+        bot.send_message(message.from_user.id, "Я в порядке! А вы?")
+    
+    else:
+        bot.send_message(message.from_user.id, "Извини, я пока очень глуп. Не понимаю о чем ты. Давай начнем сначала.")
+        
+   bot.polling(none_stop=True, interval=0)
