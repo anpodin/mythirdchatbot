3eb45c6d102aaf286a29bf81f9fee62fa94f9854
Andrey P <39740137+anpodin@users.noreply.github.com>
2018-07-07 18:34:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,18 +5,10 @@ bot = telebot.TeleBot("607967748:AAGGCS5t8TK7aKxZUeav2sgwcPTmkpVJDls")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-bot.reply_to(message, "Привет! Я бот Андрея, как твои дела?")
+bot.reply_to(message, "Привет! Как дела?")
 
-@bot.message_handler(func=lambda m: True)
+@bot.message_handler(func=lambda message: True)
 def echo_all(message):
+bot.reply_to(message, message.text)
 
-    if message.text == "Привет":
-        bot.reply_to(message, "Привет! Как твои дела?")
-    
-    elif message.text == "Как дела?" or message.text == "How are u?":
-        bot.reply_to(message, "Отлично!")
-    
-    else:
-        bot.reply_to(message, "Извини, я очень глуп, ничего не понимаю, кроме привет и как дела...")
-
-bot.polling(none_stop=True, interval=0)
+bot.polling()
