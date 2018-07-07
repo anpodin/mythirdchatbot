7e4f5b347d123e386e886abc838210a7bf24b1a9
Andrey P <39740137+anpodin@users.noreply.github.com>
2018-07-07 18:38:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ import telebot
 bot = telebot.TeleBot("607967748:AAGGCS5t8TK7aKxZUeav2sgwcPTmkpVJDls")
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-bot.reply_to(message, "Привет! Как дела?")
+    def send_welcome(message):
+        bot.reply_to(message, "Привет! Как дела?")
 
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-bot.reply_to(message, message.text)
+    def echo_all(message):
+        bot.reply_to(message, message.text)
 
 bot.polling()
