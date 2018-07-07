6393ef4acb7b478b4e5f1c6cda36894c839d6ac6
Andrey P <39740137+anpodin@users.noreply.github.com>
2018-07-07 20:31:52 +0300

    Update bot.py
    Now sending messages but not replying

--- bot.py
+++ bot.py
@@ -9,6 +9,6 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    bot.send_message(chatid, "Ой все! Хватит болтать :-D")
 
 bot.polling(none_stop=True, interval=10, timeout=30)
