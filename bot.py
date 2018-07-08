caf43d9951497d7e8a4ba67b617cbb998d912e37
Andrey P <39740137+anpodin@users.noreply.github.com>
2018-07-08 09:49:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,14 +5,20 @@ bot = telebot.TeleBot("607967748:AAGGCS5t8TK7aKxZUeav2sgwcPTmkpVJDls")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я бот Андрея, давай поговорим?")
+    bot.reply_to(message, "Привет! Я бот Андрея, давай поговорим? 🤖")
 
+@bot.message_handler(commands=['ping'])  # This is just to check if the bot is online. Nothing special
+def pong(m):
+    bot.reply_to(m, 'Pong!')
+    
+@bot.message_handler(commands=['Привет'])  # Отвечаем на Привет
+def hello(m):
+    cid = m.chat.id
+    bot.send_message(cid, 'Привет! Рад поговорить с тобой! 😋')
+    
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    cid = message.chat.id
-    if message.text = "Привет":
-    bot.send_message(cid, "Привет! Поговори со мной!")
-    elif:
-    bot.send_message(cid, "Ой все! Хватит болтать :-D")
+   cid = message.chat.id
+   bot.send_message(cid, "Ой все! Хватит болтать 😆")
 
 bot.polling(none_stop=True)
