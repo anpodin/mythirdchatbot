8cee4561b1b4e409923265d0e45815ed06c63596
Andrey P <39740137+anpodin@users.noreply.github.com>
2018-07-22 01:14:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,8 +31,8 @@ def echo_all(message):
 
 
 @bot.message_handler(content_types=['photo'])  # This one is to get an image and process it through DeepAI
-def photo(m):
-    pid = m.PhotoSize.file_id
+def photo(PhotoSize):
+    pid = PhotoSize.file_id
     path = bot.getFile(pid)
     url = "https://api.telegram.org/file/botAAGGCS5t8TK7aKxZUeav2sgwcPTmkpVJDls/" + path
     cid = m.chat.id
