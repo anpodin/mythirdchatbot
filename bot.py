ef0a27bdd08776d8f376282029bd1e17751c9533
Andrey P <39740137+anpodin@users.noreply.github.com>
2018-07-22 03:24:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ def processPhotoMessage(message):
     headers={'api-key': '104f12a5-1dae-402d-b4b6-bb24b6b501b4'}
     )
     import json
-    data = json.load(r.json())
+    data = json.loads(r.json())
     talk = data(['output'])
     
     bot.send_message(cid, talk)
