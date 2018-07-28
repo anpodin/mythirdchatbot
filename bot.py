4b1a60b01ffe1a0eba70d2d114c646f2052d1f2f
Andrey P <39740137+anpodin@users.noreply.github.com>
2018-07-28 20:43:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,8 +45,10 @@ def processPhotoMessage(message):
     headers={'api-key': '104f12a5-1dae-402d-b4b6-bb24b6b501b4'}
     )
     import json
-    talk = "Age: " + r.json()['age_range'] + "  Gender: "+ r.json()['gender'] + "  Race: " + r.json()['cultural_appearance']
-    
+    age = "Age: " + r.json()['age_range'] 
+    gender = " Gender: " + r.json()['gender'] 
+    race = " Race: " + r.json()['cultural_appearance']
+    talk = age + gender + race
     bot.send_message(cid, talk)
 
 
